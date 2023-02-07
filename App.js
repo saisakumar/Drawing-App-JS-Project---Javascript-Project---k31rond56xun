d26961e72817b1ff2907c5a2c55a3b317de0db62
sasaikumar <palthisaikumar15@gmail.com>
2023-02-07 10:18:36 +0530

    done

--- App.js
+++ App.js
@@ -82,3 +82,16 @@ function undo_last(){
         context.putImageData(restore_array[index],0,0) 
     }
 }
+
+const adviceArea = document.querySelector(".advice-slip");
+
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then(
+      (data) =>
+        (adviceArea.innerHTML = `<span class="id">${data.slip.id}.</span><h2 class="advice">${data.slip.advice}</h2>`)
+    );
+}
+
+getAdvice();
